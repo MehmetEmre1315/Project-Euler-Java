efe9813d70a2e07e4fbfb2f441ab0096d83aca57
MehmetEmre1315 <kahramanmehmetemre@gmail.com>
2024-05-09 23:47:47 +0300

    euler 026 part 2

--- euler026/src/Main.java
+++ euler026/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] fraction = new int[100];
+        int [] fraction = new int[1000];
         int numerator = 1;
         int remainder = 0;
         int [] denominator = new int[1000];
@@ -14,9 +14,9 @@ public class Main {
         int maxRecurringCycle = 0;
         int answer = 0;
 
-        for (int i = 7; i < denominator.length; i++){
+        for (int i = 7; i < denominator.length; i++) {
             int j = 0;
-            fraction = new int[100];
+            fraction = new int[1000];
             numerator = 1;
             while (j < fraction.length) {
                 while (numerator < denominator[i]) {
@@ -33,47 +33,13 @@ public class Main {
                     fraction = new int[100];
                     numerator = 1;
                     break;
-                }else {
+                } else {
                     numerator = remainder;
                     j++;
                 }
             }
 
-            while (true) {
-                recurringCycle = 0;
-                for (int k = fraction.length - 2; k >= 0; k--) {
-                    if (fraction[fraction.length - 1] == fraction[k]) {
-                        recurringCycle++;
-                        break;
-                    } else {
-                        recurringCycle++;
-                    }
-                }
-                if (recurringCycle >= 90) break;
-                if (fraction[fraction.length - 1] ==0 ) break;
-                if (fraction[fraction.length - 2] == fraction[fraction.length - (recurringCycle -2)]) {
-                    if (recurringCycle > maxRecurringCycle) {
-                        maxRecurringCycle = recurringCycle;
-                        answer = i;
-                        recurringCycle = 0;
-                        break;
-                    } else {
-                        recurringCycle = 0;
-                        break;
-                    }
-                }
-            }
+
         }
-        System.out.println(answer + " " + maxRecurringCycle);
-
-
-
-
-
-
-
-
-
-
     }
 }
