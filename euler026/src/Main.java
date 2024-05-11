84a4cb9de3f6cc5865b2b565f82fa846dd6cd312
MehmetEmre1315 <kahramanmehmetemre@gmail.com>
2024-05-11 19:58:31 +0300

    euler 026 part 5

--- euler026/src/Main.java
+++ euler026/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int [] fraction = new int[1000];
-        int numerator = 1;
-        int remainder = 0;
+        int [] fraction;
+        int numerator;
+        int remainder;
         int [] denominator = new int[1000];
 
+        // Denominators 1 to 999
         for (int i = 0; i < 1000; i++) {
             denominator[i] = i;
         }
@@ -14,24 +15,28 @@ public class Main {
         int maxRecurringCycle = 0;
         int answer = 0;
 
-        for (int i = 7; i < denominator.length; i++) {
+        // Loop through all denominators
+        // Output fraction value goes into array
+        for (int i = 0; i < denominator.length; i++) {
             int j = 0;
-            fraction = new int[1000];
+            int l = 0;
+            fraction = new int[10000];
             numerator = 1;
             while (j < fraction.length) {
                 while (numerator < denominator[i]) {
+                    l++;
                     numerator *= 10;
-                    if (numerator >= 100 && j < fraction.length - 1) {
+                    if (l >= 2 && j < fraction.length - 1) {
                         j++;
                     }
                 }
+                l = 0;
 
                 fraction[j] = numerator / denominator[i];
                 remainder = numerator % denominator[i];
 
                 if (remainder == 0) {
-                    fraction = new int[1000];
-                    numerator = 1;
+                    fraction = new int[10000];
                     break;
                 } else {
                     numerator = remainder;
@@ -39,29 +44,45 @@ public class Main {
                 }
             }
 
+            // To eliminate recurring 0's at the end of the fraction
             if (
-                    fraction[fraction.length - 1] == fraction[fraction.length - 100]
-                    && fraction[fraction.length - 1] == 0
+                    fraction[fraction.length - 1] == 0 &&
+                    fraction[fraction.length - 2] == 0 &&
+                    fraction[fraction.length - 3] == 0 &&
+                    fraction[fraction.length - 4] == 0 &&
+                    fraction[fraction.length - 5] == 0
+
             ) continue;
 
+            // Loop through the fraction to find recurring cycle
+            for (int k = fraction.length - 2; k > 0; k--) {
+                if (
+                        //fraction[fraction.length - 1] == fraction[k] &&
+                        fraction[fraction.length - 2] == fraction[k - 1] &&
+                        fraction[fraction.length - 3] == fraction[k - 2] &&
+                        fraction[fraction.length - 4] == fraction[k - 3] &&
+                        fraction[fraction.length - 5] == fraction[k - 4]
+                ) {
+                    recurringCycle ++;
+                    // To see every single recurring cycle
+                    System.out.println("Number "+ i + " has recurring cycle of " + recurringCycle);
+                    if (recurringCycle > maxRecurringCycle) {
+                        maxRecurringCycle = recurringCycle;
+                        answer = i;
+                    }
+                    recurringCycle = 0;
+                    break;
+                } else {
+                    recurringCycle ++;
+                }
+                if (k < 20) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    recurringCycle = 0;
+                    break;
+                }
+            }
         }
 
+        System.out.println("\nAnswer is number " + answer + " with recurring cycle of " + maxRecurringCycle);
     }
 }
