d67cf77c306bf6823809338434e0a747f2d2b1a6
MehmetEmre1315 <kahramanmehmetemre@gmail.com>
2024-05-27 14:50:01 +0300

    new file system part 2

--- AnswerTest.java
+++ AnswerTest.java
@@ -3,5 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnswerTest {
     @Test
-    public void testEuler001 () {assertEquals(233168, new ProjectEuler001().run());}
+    public void testProjectEuler001 () {assertEquals(233168, new ProjectEuler001().run());}
+    @Test
+    public void ProjectEuler002 () {assertEquals(4613732, new ProjectEuler002().run());}
 }
